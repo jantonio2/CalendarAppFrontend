0b2214750e3783d0288446e4252de01267e5647d
jantonio2 <jesusrochavedia@gmail.com>
2021-11-21 16:11:25 -0400

    Personalizando el evento y disparando distintos eventos con click

--- src/Components/calendar/CalendarScreen.js
+++ src/Components/calendar/CalendarScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/es';
 import { Navbar } from '../ui/Navbar';
 import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { withRouter } from 'react-router';
 
 moment.locale('es');
 
@@ -15,11 +15,31 @@ const events = [{
   title: 'Cumpleaños del Bisho',
   start: moment().toDate(),
   end: moment().add(2, 'hours').toDate(),
-  bgcolor: '#fafafa'
+  bgcolor: '#fafafa',
+  notes: ['Comprar Torta'],
+  user: {
+    _id: '123',
+    name: 'Antonio'
+  }
 }];
 
 export const CalendarScreen = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+  const onDoubleClick = (e) => {
+    console.log(e);
+  };
+
+  const onSelectEvent = (e) => {
+    console.log(e);
+  };
   
+  const onViewChange = (e) => {
+    setLastView(e)
+    localStorage.setItem('lastView', e);
+  };
+
   const eventStyleGetter = (event, start, end , isSelected) => {
     
     const style = {
@@ -46,6 +66,13 @@ export const CalendarScreen = () => {
         endAccessor="end"
         messages={messages}
         eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view= {lastView}
+        components={{
+          event: CalendarEvent
+        }}
       />
     </div>
   );
