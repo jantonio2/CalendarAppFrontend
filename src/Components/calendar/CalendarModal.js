f43460acfaa10e0e32d19ce8777839aea1f0411d
jantonio2 <jesusrochavedia@gmail.com>
2021-12-05 15:20:54 -0400

    Creando un evento en la base de datos

--- src/Components/calendar/CalendarModal.js
+++ src/Components/calendar/CalendarModal.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 import Swal from 'sweetalert2';
 import { useDispatch, useSelector } from 'react-redux';
 import { uiCloseModal } from '../../actions/ui';
-import { eventAddNew, eventClearActiveEvent, eventUpdated } from '../../actions/events';
+import { eventClearActiveEvent, eventStartAddNew, eventUpdated } from '../../actions/events';
 
 const customStyles = {
   content: {
@@ -93,14 +93,7 @@ export const CalendarModal = () => {
     if(activeEvent){
       dispatch(eventUpdated(formValues));
     } else {
-      dispatch(eventAddNew({
-        ...formValues, 
-        id: new Date().getTime(),
-        user: {
-          id: '123',
-          name: 'ElAnto'
-        }
-      }));
+      dispatch(eventStartAddNew( formValues ));
     }
 
     setTitleValid(true);
