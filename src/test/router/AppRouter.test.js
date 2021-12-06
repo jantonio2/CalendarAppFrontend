8dece3eb02291ff5e9d7c3a990bbcda1093fdd7b
jantonio2 <jesusrochavedia@gmail.com>
2021-12-06 15:48:17 -0400

    Pruebas en el componente AppRouter

---
+++ src/test/router/AppRouter.test.js
@@ -0,0 +1,31 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { AppRouter } from './../../router/AppRouter';
+
+const middlewares = [ thunk ];
+const mockStore = configureStore( middlewares );
+
+describe('Pruebas en <AppRouter />', () => {
+  
+  test('debe de mostrar el Espere...', () => {
+    const initState = {
+      auth: {
+        checking: true
+      }
+    };
+    
+    const store = mockStore( initState );
+    
+    const wrapper = mount(
+      <Provider store={ store }>
+        <AppRouter />
+      </Provider>
+    ); 
+    
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('h5').exists()).toBeTruthy();
+  });
+  
+});
