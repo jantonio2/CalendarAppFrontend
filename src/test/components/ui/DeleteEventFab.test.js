a2a29027580976e5c130684c2adc7442a5773e9f
jantonio2 <jesusrochavedia@gmail.com>
2021-12-06 15:38:17 -0400

    Pruebas en el componente DeleteEventFab. Llamando la accion eventStartDelete

--- src/test/components/ui/DeleteEventFab.test.js
+++ src/test/components/ui/DeleteEventFab.test.js
@@ -3,6 +3,11 @@ import thunk from 'redux-thunk';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { DeleteEventFab } from './../../../components/ui/DeleteEventFab';
+import { eventStartDelete } from '../../../actions/events';
+
+jest.mock('../../../actions/events', () => ({
+  eventStartDelete: jest.fn()
+}));
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -22,6 +27,12 @@ describe('Pruebas en <DeleteEventFab />', () => {
 
   test('debe de mostrarse correctamente', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  
+  test('debe de llamar el eventStartDelete al hacer click', () => {
+    wrapper.find('.btn').prop('onClick')();
+
+    expect(eventStartDelete).toHaveBeenCalled();
   });
   
 });
