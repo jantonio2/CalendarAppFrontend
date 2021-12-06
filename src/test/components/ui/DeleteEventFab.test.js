51879e7b1749396eab103389edc615b8557d9725
jantonio2 <jesusrochavedia@gmail.com>
2021-12-06 13:59:12 -0400

    Pruebas en el componente DeleteEventFab

---
+++ src/test/components/ui/DeleteEventFab.test.js
@@ -0,0 +1,27 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { DeleteEventFab } from './../../../components/ui/DeleteEventFab';
+
+const middlewares = [ thunk ];
+const mockStore = configureStore( middlewares );
+
+const initState = {};
+
+const store = mockStore( initState );
+store.dispatch = jest.fn();
+
+const wrapper = mount(
+  <Provider store={ store }>
+    <DeleteEventFab />
+  </Provider>
+);
+
+describe('Pruebas en <DeleteEventFab />', () => {
+
+  test('debe de mostrarse correctamente', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+});
