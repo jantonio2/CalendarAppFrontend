ce851d2e1bb7cdbb9fd85a8315dc83db400e52f9
jantonio2 <jesusrochavedia@gmail.com>
2021-12-06 20:57:43 -0400

    Probando la accion de crear un nuevo evento

--- src/test/components/calendar/CalendarModal.test.js
+++ src/test/components/calendar/CalendarModal.test.js
@@ -4,11 +4,18 @@ import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { CalendarModal } from './../../../components/calendar/CalendarModal';
 import moment from 'moment';
-import { eventClearActiveEvent, eventStartUpdate } from '../../../actions/events';
+import { eventClearActiveEvent, eventStartUpdate, eventStartAddNew } from '../../../actions/events';
+import { act } from '@testing-library/react';
+import Swal from 'sweetalert2';
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn()
+}));
 
 jest.mock('../../../actions/events', () => ({
   eventStartUpdate: jest.fn(),
   eventClearActiveEvent: jest.fn(),
+  eventStartAddNew: jest.fn()
 }));
 
 const middlewares = [ thunk ];
@@ -70,6 +77,72 @@ describe('Pruebas en el componente <CalendarModal />', () => {
     });
 
     expect(wrapper.find('input[name="title"]').hasClass('is-invalid')).toBeTruthy();
+  });
+  
+  test('debe de crear un nuevo evento', () => {
+    const initState = {
+      calendar: {
+        events: [],
+        activeEvent: null
+      },
+      auth: {
+        checking: false,
+        uid: 'TESTING',
+        name: 'test'
+      },
+      ui: {
+        modalOpen: true
+      } 
+    };
+    
+    const store = mockStore( initState );
+    store.dispatch = jest.fn();
+    
+    const wrapper = mount(
+      <Provider store={ store }>
+        <CalendarModal />
+      </Provider>
+    );
+
+    wrapper.find('input[name="title"]').simulate('change', {
+      target: {
+        name: 'title',
+        value: 'Hola pruebas'
+      }
+    });
+
+    wrapper.find('form').simulate('submit', {
+      preventDefault(){}
+    });
+
+    expect(eventStartAddNew).toHaveBeenCalledWith({
+      end: expect.anything(),
+      start: expect.anything(),
+      title: 'Hola pruebas',
+      notes: ''
+    });
+
+    expect(eventClearActiveEvent).toHaveBeenCalled();
+  });
+
+  test('debe de validar las fechas', () => {
+    wrapper.find('input[name="title"]').simulate('change', {
+      target: {
+        name: 'title',
+        value: 'Hola pruebas'
+      }
+    });
+
+    const today = new Date();
+    act(() => {
+      wrapper.find('DateTimePicker').at(1).prop('onChange')(today);
+    });
+
+    wrapper.find('form').simulate('submit', {
+      preventDefault(){}
+    });
+
+    expect(Swal.fire).toHaveBeenCalledWith('Error', 'La fecha fin debe ser mayor a la fecha de inicio', 'error');
   });
   
 });
