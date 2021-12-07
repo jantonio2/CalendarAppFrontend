a334b7bd325a7bfd7e080fd3fe0399ad717d1980
jantonio2 <jesusrochavedia@gmail.com>
2021-12-06 20:00:53 -0400

    Pruebas en el componente del CalendarScreen

--- src/test/components/calendar/CalendarScreen.test.js
+++ src/test/components/calendar/CalendarScreen.test.js
@@ -3,6 +3,17 @@ import thunk from 'redux-thunk';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { CalendarScreen } from './../../../components/calendar/CalendarScreen';
+import { messages } from '../../../helpers/calendar-messages-es';
+import { types } from '../../../types/types';
+import { eventSetActive } from '../../../actions/events';
+import { act } from 'react-dom/test-utils';
+
+jest.mock('../../../actions/events', () => ({
+  eventSetActive: jest.fn(),
+  eventStartLoading: jest.fn()
+}));
+
+Storage.prototype.setItem = jest.fn();
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -37,4 +48,23 @@ describe('Pruebas en el componente <CalendarScreen />', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  test('pruebas con las interacciones del calendario', () => {
+    const calendar = wrapper.find('Calendar');
+
+    const calendarMessages = calendar.prop('messages');
+    expect(calendarMessages).toEqual(messages);
+
+    calendar.prop('onDoubleClickEvent')();
+    expect(store.dispatch).toHaveBeenCalledWith({ type: types.uiOpenModal });
+    
+    // haciendo de otra forma
+    calendar.prop('onSelectEvent')({ start: 'Now' });
+    expect(eventSetActive).toHaveBeenCalledWith({ start: 'Now' });
+
+    act(() => {
+      calendar.prop('onView')('week');
+      expect(localStorage.setItem).toHaveBeenCalledWith('lastView', 'week');
+    });
+  });
+  
 });
